8fc3ee657d8163d26cccfc4babc8939f54dec618
SeongJin Kim <dge03078@gmail.com>
2020-12-08 03:53:43 +0900

    3 - Fix the nav circle button fixed bug and add function
    Fix the nav circle button fixed postion bug
    Add function which click content makes close the nav button

--- 3-Rotating Navigation/script.js
+++ 3-Rotating Navigation/script.js
@@ -2,6 +2,15 @@ const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
-open.addEventListener('click', () => container.classList.add('show-nav'))
+open.addEventListener('click', show_nav)
 
-close.addEventListener('click', () => container.classList.remove('show-nav'))
+close.addEventListener('click', hide_nav)
+
+function show_nav() {
+  container.classList.add('show-nav')
+  container.addEventListener('click', hide_nav, true)
+}
+
+function hide_nav() {
+  container.classList.remove('show-nav')
+}
